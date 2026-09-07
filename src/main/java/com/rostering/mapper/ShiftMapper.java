2116ca365f20d2e6b2fd27a2e962771f2c4040dc
mikeleworkshop <Mike.lemanh@gmail.com>
2026-09-07 21:56:55 +0930

    Implement shift CRUD API methods

--- src/main/java/com/rostering/mapper/ShiftMapper.java
+++ src/main/java/com/rostering/mapper/ShiftMapper.java
@@ -2,6 +2,12 @@ package com.rostering.mapper;
 
 import org.mapstruct.Mapper;
 
+import com.rostering.model.dto.ShiftRequestDTO;
+import com.rostering.model.dto.ShiftResponseDTO;
+import com.rostering.model.entity.Shift;
+
 @Mapper(componentModel = "spring")
 public interface ShiftMapper {
+	Shift toEntity(ShiftRequestDTO dto);
+	ShiftResponseDTO toResponseDTO(Shift shift);
 }
--- src/main/java/com/rostering/model/dto/ShiftRequestDTO.java
+++ src/main/java/com/rostering/model/dto/ShiftRequestDTO.java
@@ -1,9 +1,18 @@
 package com.rostering.model.dto;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-
 import jakarta.validation.constraints.NotBlank;
 
-public record ShiftRequestDTO() {
+public record ShiftRequestDTO(
+		@NotBlank(message = "Shift's Name is required")
+		String name,
+		
+		@NotBlank(message = "Shift's Date must be in the future")
+		String date,
+		
+		@NotBlank(message = "Start Time is required")
+		String startTime,
+		
+		@NotBlank(message = "End Time is required")
+		String endTime
+		) {
 }
