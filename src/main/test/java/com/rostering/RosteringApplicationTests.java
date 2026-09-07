ac9ef82fa74bee37bb70e90161a2492d366b4640
Mike Le <Mike.lemanh@gmail.com>
2026-09-07 12:45:04 +0000

    Add missing test file

---
+++ src/main/test/java/com/rostering/RosteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rostering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosteringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
